c60bf5cc38efbf942bfa06c9481133fa3891221d
Alex <voyevudko.o@gmail.com>
2020-08-31 18:10:03 +0300

    Set up Ball and Camera controllers. Added sounds to the ball and created simple particle effect to play on collision.

---
+++ Helix-jump/Assets/Scripts/Ball/BallController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	[Header("Impulse Settings")]
+	[SerializeField] private Rigidbody rb;
+	[SerializeField] private float impulseForce = 5f;
+
+	[Header("Audio Settings")]
+	[SerializeField] private AudioSource playAudio;
+	[SerializeField] private AudioClip hitSound;
+	[SerializeField] private AudioClip deadSound;
+
+
+	private bool ignoreNextCollision;
+	private Vector3 startPosition;
+
+	private void Awake()
+	{
+		startPosition = transform.position;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (ignoreNextCollision)
+			return;
+
+		if (collision.gameObject.CompareTag("Good Part"))
+		{
+			rb.velocity = Vector3.zero;
+			rb.AddForce(Vector3.up * impulseForce, ForceMode.Impulse);
+			playAudio.PlayOneShot(hitSound);
+		}
+
+		if (collision.gameObject.CompareTag("Dead Part"))
+		{
+			// Call the GameManager method to reset the level.
+			playAudio.PlayOneShot(deadSound);
+		}
+		
+		ignoreNextCollision = true;
+		Invoke("AllowCollision", 0.2f);
+	}
+
+	// Used to exclude the effect of multiple collisions.
+	private void AllowCollision()
+	{
+		ignoreNextCollision = false;
+	}
+
+	// Should be used in the GameManager to reset the level.
+	public void ResetBallPosition()
+	{
+		transform.position = startPosition;
+	}
+}
---
+++ Helix-jump/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	[SerializeField] private Transform player;
+
+	private Vector3 defaultPos;
+	private Vector3 tempPos;
+	private float deltaY;
+
+	private void Awake()
+	{
+		defaultPos = transform.position;
+		tempPos = transform.position;
+		deltaY = player.position.y - tempPos.y;
+	}
+
+	private void LateUpdate()
+	{
+		float playerY = player.position.y;
+
+		if (transform.position.y + deltaY > playerY)
+		{
+			tempPos.y = playerY - deltaY;
+			transform.position = tempPos;
+		} 
+	}
+
+	// Should be used in the GameManager to reset the level.
+	public void ResetCameraPosition()
+	{
+		transform.position = defaultPos;
+	}
+}
