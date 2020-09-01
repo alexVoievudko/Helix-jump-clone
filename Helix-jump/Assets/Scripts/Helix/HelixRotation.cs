97019721d40bc069b5d62b75f534fdc0116c25cb
Mykola-Kvitka <nikolakvitka@gmail.com>
2020-09-01 15:55:39 +0300

    add Helix Set up
    add Helix Set up

---
+++ Helix-jump/Assets/Scripts/Helix/HelixRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixRotation : MonoBehaviour
+{
+    private Vector2 lastTapPos;
+
+    private Vector3 startRotation;
+    
+    void Awake()
+    {
+        startRotation = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            Vector2 curPos = Input.mousePosition;
+
+            if (lastTapPos == Vector2.zero)
+            {
+                lastTapPos = curPos;
+            }
+
+            float delta = lastTapPos.x - curPos.x;
+            lastTapPos = curPos;
+
+            transform.Rotate(Vector3.up * delta);
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastTapPos = Vector2.zero;
+        }
+    }
+}
---
+++ Helix-jump/Assets/Scripts/ScriptableObjects/Stage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class Level {
+    [Range(1, 11)]
+    public int partCount = 11;
+
+    [Range(1, 11)]
+    public int deathPartCount = 1;
+}
+
+[CreateAssetMenu(fileName ="New Stage")]
+public class Stage : ScriptableObject
+{
+    public Color stageBackgroundColor = Color.white;
+    public Color stageLevelpartColor = Color.white;
+    public Color stageBallColor = Color.white;
+    public List<Level> levels = new List<Level>; 
+}
